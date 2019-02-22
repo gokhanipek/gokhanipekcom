9e2b944b3ae90dc95edcc3a3550507d65744141a
Gokhan <gokhan.ipek@etiya.com>
2019-02-22 16:56:51 +0300

    second

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  ngOnInit() {}
+export class HomeComponent {
+  tabIntro = false;
+  tabAbout = false;
+  tabContact = false;
+  main = false;
+  header = true;
+  openIntro() {
+    this.tabIntro = true;
+    this.tabAbout = false;
+    this.tabContact = false;
+    this.main = true;
+    this.header = false;
+  }
+  openAbout() {
+    this.tabIntro = false;
+    this.tabAbout = true;
+    this.tabContact = false;
+    this.main = true;
+    this.header = false;
+  }
+  openContact() {
+    this.tabIntro = false;
+    this.tabAbout = false;
+    this.tabContact = true;
+    this.main = true;
+    this.header = false;
+  }
 }

d19efb88a48896342d4ed3ea1692e58d971e9566
Gokhan <gokhan.ipek@etiya.com>
2019-02-25 15:57:23 +0300

    divided in parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IntroComponent } from './home/intro/intro.component';
+import { StoryComponent } from './home/story/story.component';
+import { BlogComponent } from './home/blog/blog.component';
+import { ContactComponent } from './home/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, IntroComponent, StoryComponent, BlogComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/home/intro/intro.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.scss'],
+})
+export class IntroComponent {}

e188d6e225918321b7c3f55a340222c899ef9ae6
unknown <gokhan.ipek@etiya.com>
2019-07-07 22:11:02 +0300

    'venusadded'

--- src/app/home/webgl/webgl.component.ts
+++ src/app/home/webgl/webgl.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular
 import * as THREE from 'three';
 
 
+
 @Component({
   selector: 'webgl-component',
   template: `
@@ -19,32 +20,49 @@ export class WebglComponent {
   scene = null;
   camera = null;
   mesh = null;
+  directionalLight = null;
 
   constructor() {
+      this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+    
       this.scene = new THREE.Scene();
-
-      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+      
       this.camera.position.z = 1000;
 
-      var geometry = new THREE.SphereGeometry( 50, 320, 320 );
-      var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-      var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 )
-      this.mesh = new THREE.Mesh(geometry, material );
+      var ambientLight =new THREE.AmbientLight( 0xffffff, 1 );// soft white light
+      
+      ambientLight.position.z = 1;
+      ambientLight.castShadow = true;
 
-      this.scene.add(this.mesh, directionalLight);
+
+      
+      this.scene.add(ambientLight);
+
+      var geometry = new THREE.SphereGeometry( 100, 320,320 );
+
+      var material = new THREE.MeshLambertMaterial( {map: new THREE.TextureLoader().load('../../../assets/images/venus.jpg')});
+      
+      this.mesh = new THREE.Mesh(geometry, material );
+      
+      this.mesh.position.z = -100;
+
+
+
+      this.scene.add(this.mesh);
   }
 
   ngAfterViewInit() {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+
       this.animate();
   }
 
   animate() {
       window.requestAnimationFrame(() => this.animate());
-      this.mesh.rotation.x += 0.01;
-      this.mesh.rotation.y += 0.02;
-      this.mesh.rotation.z += 0.01
+      this.mesh.rotation.x += 0.001;
+      this.mesh.rotation.y += 0.002;
+      this.mesh.rotation.z += 0.002;
       this.renderer.render(this.scene, this.camera);
   }
 

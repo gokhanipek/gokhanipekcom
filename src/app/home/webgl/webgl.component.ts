3215dc37ac2f2e36ba3528a86da9f1d2760dc2e9
unknown <gokhan.ipek@etiya.com>
2019-07-08 00:12:31 +0300

    'threejs-progress'

--- src/app/home/webgl/webgl.component.ts
+++ src/app/home/webgl/webgl.component.ts
@@ -21,6 +21,7 @@ export class WebglComponent {
   camera = null;
   mesh = null;
   directionalLight = null;
+  controls;
 
   constructor() {
       this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
@@ -40,13 +41,13 @@ export class WebglComponent {
 
       var geometry = new THREE.SphereGeometry( 100, 320,320 );
 
-      var material = new THREE.MeshLambertMaterial( {map: new THREE.TextureLoader().load('../../../assets/images/venus.jpg')});
+      var material = new THREE.MeshPhongMaterial({
+           map: new THREE.TextureLoader().load('../../../assets/images/venus.jpg'), 
+          });
       
       this.mesh = new THREE.Mesh(geometry, material );
       
       this.mesh.position.z = -100;
-
-
 
       this.scene.add(this.mesh);
   }
@@ -54,6 +55,8 @@ export class WebglComponent {
   ngAfterViewInit() {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    
+
 
       this.animate();
   }

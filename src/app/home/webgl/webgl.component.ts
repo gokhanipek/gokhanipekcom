df24435a8bd1063f781bb0bd624baf10f2de59db
gokhan ipek <gokhan.ipek@etiya.com>
2019-07-08 10:58:18 +0300

    controls added

--- src/app/home/webgl/webgl.component.ts
+++ src/app/home/webgl/webgl.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three-orbitcontrols-ts';
 
+
+
+declare const require: (moduleId: string) => any;
 
 
 @Component({
@@ -21,41 +25,36 @@ export class WebglComponent {
   camera = null;
   mesh = null;
   directionalLight = null;
-  controls;
+
 
   constructor() {
-      this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-    
-      this.scene = new THREE.Scene();
-      
-      this.camera.position.z = 1000;
 
-      var ambientLight =new THREE.AmbientLight( 0xffffff, 1 );// soft white light
-      
-      ambientLight.position.z = 1;
-      ambientLight.castShadow = true;
+    this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.camera.position.set( -200, -100, 500 )
+
+    this.scene = new THREE.Scene();
+    var background = new THREE.TextureLoader().load('../../../assets/images/stars.jpg');
+    this.scene.background = background;
+    this.camera.position.z = 1000;
+    var ambientLight = new THREE.AmbientLight( 0xffffff, 1 );// soft white light
+    ambientLight.position.z = 1;
+    this.scene.add(ambientLight);
+    var geometry = new THREE.SphereGeometry( 100, 320,320 );
+    var material = new THREE.MeshPhongMaterial({
+         map: new THREE.TextureLoader().load('../../../assets/images/venus.jpg'),
+        });
+    this.mesh = new THREE.Mesh(geometry, material );
+    this.mesh.position.z = -100;
+    this.scene.add(this.mesh);
 
 
-      
-      this.scene.add(ambientLight);
-
-      var geometry = new THREE.SphereGeometry( 100, 320,320 );
-
-      var material = new THREE.MeshPhongMaterial({
-           map: new THREE.TextureLoader().load('../../../assets/images/venus.jpg'), 
-          });
-      
-      this.mesh = new THREE.Mesh(geometry, material );
-      
-      this.mesh.position.z = -100;
-
-      this.scene.add(this.mesh);
   }
 
   ngAfterViewInit() {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
-    
+
 
 
       this.animate();
